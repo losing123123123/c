d310ba8086fc34d2b0773a177de9265bb66efc90
losing123123123 <106807901+losing123123123@users.noreply.github.com>
2022-10-07 13:20:14 +0800

    Add files via upload

---
+++ 202209/20220919/main.c
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+
+/*
+int main(void) {
+    int a[10] = { 0,1,2,3,4,5,6,7,8,9 }, * p = a + 3;
+    printf("%d" ,*++p);
+    return 0;
+}
+
+int main(void) {
+    int n;
+    char y[10] = "ntse";
+    char* x = y;
+    n = strlen(x);
+    *x = x[n];
+    x++;
+    printf("x=%s,", x);
+    printf("y=%s\n", y);
+    return 0;
+}
+
+*/
+
+int main()
+{
+
+    return 0;
+}
