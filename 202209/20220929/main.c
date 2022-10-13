f05829973c8ef8e8f41aacf23f7bceca8230f2e1
losing123123123 <106807901+losing123123123@users.noreply.github.com>
2022-10-13 10:16:59 +0800

    Add files via upload

---
+++ 202209/20220929/main.c
@@ -0,0 +1,19 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int add(int x, int y)
+{
+	return x + y;
+}
+int main()
+{
+	int (*pf)(int, int) = add;
+
+	printf("%d\n", add(3, 5));
+
+	printf("%d\n", pf(3, 5));
+
+	printf("%d\n", (*pf)(3, 5));
+
+	return 0;
+}
