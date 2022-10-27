b25152302b238fc9a401fe340ae5cbbba1eabfce
losing123123123 <106807901+losing123123123@users.noreply.github.com>
2022-10-27 12:21:57 +0800

    Add files via upload

--- 202210/1011/main.c
+++ 202210/1011/main.c
@@ -1,7 +1,14 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
+#include<string.h>
 int main()
 {
+	char a[] = "abcdef";
+	char b[] = { 'a','b','c','d','e','f' };
+	printf("%d\t%d\n", sizeof(a), sizeof(b));
+	printf("%d\t%d\n", strlen(a), strlen(b));
 
-	return 0;
+	int arr[] = { 1,2,3,4,5,6 };
+	printf("%d\n", sizeof(arr));
+	return 0; 
 }
