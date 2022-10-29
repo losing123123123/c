34132b37d7d6849bdb75e4d7930f0a51417c407e
losing123123123 <106807901+losing123123123@users.noreply.github.com>
2022-10-29 12:14:55 +0800

    Add files via upload

--- 202210/1016/main.c
+++ 202210/1016/main.c
@@ -1,8 +1,27 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 
+//7个分数，去掉最高分和最低分，计算平均分
 int main()
 {
-
+	int sum = 0;
+	int max = 0;
+	int min = 100;
+	int num = 0;
+	int i = 0;
+	for (i = 0; i < 7; i++)
+	{
+		scanf("%d", &num);
+		sum += num;
+		if (num > max)
+		{
+			max = num;
+		}
+		if (num < min)
+		{
+			min = num;
+		}
+	}
+	printf("%.2f", (sum - max - min) / 5.0);
 	return 0;
 }
