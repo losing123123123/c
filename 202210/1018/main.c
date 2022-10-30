484ede632d9df25d0221285ca080fa8914f56569
losing123123123 <106807901+losing123123123@users.noreply.github.com>
2022-10-30 17:39:21 +0800

    Add files via upload

--- 202210/1018/main.c
+++ 202210/1018/main.c
@@ -1,9 +1,37 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 
+//顺序插入一个数字
 int main()
 {
+	int n = 0;
+	scanf("%d", &n);
+	int i = 0;
+	int arr[20] = { 0 };
+	for (i = 0; i < n; i++)
+	{
+		scanf("%d", &arr[i]);
+	}
+	int m = 0;
+	scanf("%d", &m);
+	for (i = n - 1; i >= 0; i--)
+	{
+		if (arr[i] > m)
+		{
+			arr[i + 1] = arr[i];
+		}
+		else
+		{
+			break;
+		}
+	}
 
+	arr[i + 1] = m;
+
+	for (i = 0; i < n+1; i++)
+	{
+		printf("%d ", arr[i]);
+	}
 
 	return 0;
 }
