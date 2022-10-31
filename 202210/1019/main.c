d19bf3a703399a6fe7a37158e939e1707f7539b0
losing123123123 <106807901+losing123123123@users.noreply.github.com>
2022-10-31 10:29:30 +0800

    Add files via upload

--- 202210/1019/main.c
+++ 202210/1019/main.c
@@ -1,7 +1,27 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 
+//É¾³ý×Ö·û´®ÖÐµÄ³ý×ÖÄ¸ÍâµÄ×Ö·û¡£
+
 int main()
 {
-	return 0;
+    char line[150];
+    int i, j;
+    printf("ÊäÈëÒ»¸ö×Ö·û´®: ");
+    fgets(line, (sizeof line / sizeof line[0]), stdin);
+
+    for (i = 0; line[i] != '\0'; ++i)
+    {
+        while (!((line[i] >= 'a' && line[i] <= 'z') || (line[i] >= 'A' && line[i] <= 'Z') || line[i] == '\0'))
+        {
+            for (j = i; line[j] != '\0'; ++j)
+            {
+                line[j] = line[j + 1];
+            }
+            line[j] = '\0';
+        }
+    }
+    printf("Êä³ö: ");
+    puts(line);
+    return 0;
 }
