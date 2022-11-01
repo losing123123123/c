41e3a6afb771fc63bc719164b2d46b3c73b5223d
losing123123123 <106807901+losing123123123@users.noreply.github.com>
2022-11-01 17:24:58 +0800

    Add files via upload

--- 202210/1021/main.c
+++ 202210/1021/main.c
@@ -1,7 +1,27 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 
+
+//二进制转换为十进制
+int convertBinaryToDecimal(long long n);
+
 int main()
 {
-	return 0;
+    long long n;
+    printf("输入一个二进制数: ");
+    scanf("%lld", &n);
+    printf("二进制数 %lld 转换为十进制为 %d", n, convertBinaryToDecimal(n));
+    return 0;
+}
+int convertBinaryToDecimal(long long n)
+{
+    int decimalNumber = 0, i = 0, remainder;
+    while (n != 0)
+    {
+        remainder = n % 10;
+        n /= 10;
+        decimalNumber += remainder * pow(2, i);
+        ++i;
+    }
+    return decimalNumber;
 }
