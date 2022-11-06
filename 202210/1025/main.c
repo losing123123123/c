eea24428e86dfe7da41c5f8b4facff38f584a275
losing123123123 <106807901+losing123123123@users.noreply.github.com>
2022-11-06 21:52:57 +0800

    Add files via upload

--- 202210/1025/main.c
+++ 202210/1025/main.c
@@ -3,7 +3,23 @@
 
 int main()
 {
+	FILE* pf = fopen("text.txt", "r");//r为读，w为写
+	if (pf == NULL)
+	{
+		perror("fopen");
+	}
+	else
+	{
+		//写入
+		//fputc('h', pf);
+		//fputc('e', pf);
 
+		//读取
+		printf("%c", fgetc(pf));
+		printf("%c", fgetc(pf));
+	}
+	fclose(pf);
+	pf = NULL;
 
 	return 0;
 }
