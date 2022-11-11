1eac97bdf2e8715886c4dfca49bd4870b7acc54e
losing123123123 <106807901+losing123123123@users.noreply.github.com>
2022-11-11 12:52:39 +0800

    Add files via upload

--- 202211/05/main.c
+++ 202211/05/main.c
@@ -1,9 +1,49 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 
+//递归写出n的阶乘
+
+int A(int n)
+{
+	
+	if (n == 1)
+	{
+		return 1;
+	}
+	else
+		return n * A(n - 1);
+}
+
+
+//利用递归反向打印字符串
+void B(char* p)
+{
+	if (*(p + 1) != '\0')
+	{
+		B(p + 1);
+		printf("%c", *p);
+	}
+	else
+		printf("%c", *p);
+}
+
+//有5个人，第一个人10岁，后面每个人比前一个人大2岁，求第五个人多少岁
+int C(int n)
+{
+	if (n == 1)
+	{
+		return 10;
+	}
+	else
+		return C(n - 1) + 2;
+}
+
+
 int main()
 {
+	//printf("%d", A(5));
 
-
+	//B("abcdef");
+	printf("%d", C(5));
 	return 0;
 }
