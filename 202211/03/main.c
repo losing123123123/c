f6a1cff765037be1ce1278f5a18db36b286d2837
losing123123123 <106807901+losing123123123@users.noreply.github.com>
2022-11-11 12:51:24 +0800

    Add files via upload

--- 202211/03/main.c
+++ 202211/03/main.c
@@ -1,16 +1,34 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 
+//兔子生崽问题
 int main()
 {
-
-	for ()
+	int i = 0;
+	int sum = 0;
+	int num = 0;
+	int temp = 0;
+	for (i = 1; i <= 40; i++)
 	{
-		int n;
-	}
-	for ()
-	{
+		if (i == 1 || i == 2)
+		{
+			sum = 1;
+		}
+		else if (i == 3)
+		{
+			num = 1;
+			sum = sum + num;
+			temp = sum;
 
+		}
+		else
+		{
+			temp = sum;
+			sum += num;
+			num = temp;
+			
+		}
+		printf("第%d个月的兔子总数为：%d对\n", i,sum);
 	}
 	return 0;
 }
