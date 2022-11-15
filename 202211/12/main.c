dd9a0c6be6d52f70711ce5b565fca3aee0e49da6
losing123123123 <106807901+losing123123123@users.noreply.github.com>
2022-11-15 13:42:55 +0800

    Add files via upload

--- 202211/12/main.c
+++ 202211/12/main.c
@@ -1,8 +1,28 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
+#include <stdio.h>
+
+int Count_Digit(const int N, const int D);
+
 int main()
 {
+    int N, D;
 
+    scanf("%d %d", &N, &D);
+    printf("%d\n", Count_Digit(N, D));
+    printf("%d\n", N%10);
+    return 0;
+}
 
-	return 0;
+/* 你的代码将被嵌在这里 */
+int Count_Digit(const int N, const int D)
+{
+    int M = N;
+    int count = 0;
+    while (M)
+    {
+        if (M % 10 == D || M % 10 == -D)
+            count++;
+        M = M / 10;
+    }
+    return count;
 }
