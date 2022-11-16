5269eff381e9a0b83b8d0f6c0ea111349e4c59a1
losing123123123 <106807901+losing123123123@users.noreply.github.com>
2022-11-16 14:09:01 +0800

    Add files via upload

--- 202211/13/main.c
+++ 202211/13/main.c
@@ -1,9 +1,42 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
+
+#include <iostream>
+using namespace std;
+
+#define MAXSIZE 50
+typedef int KeyType;
+
+typedef  struct
+{
+    KeyType  key;
+} ElemType;
+
+typedef  struct
+{
+    ElemType* R;
+    int  length;
+} SSTable;
+
+void  Create(SSTable& T)
+{
+    int i;
+    T.R = new ElemType[MAXSIZE + 1];
+    cin >> T.length;
+    for (i = 1; i <= T.length; i++)
+        cin >> T.R[i].key;
+}
+
+int  Search_Bin(SSTable T, KeyType k);
 
 int main()
 {
-
-
-	return 0;
+    SSTable T;  KeyType k;
+    Create(T);
+    cin >> k;
+    int pos = Search_Bin(T, k);
+    if (pos == 0) cout << "NOT FOUND" << endl;
+    else cout << pos << endl;
+    return 0;
 }
+
+/* 请在这里填写答案 */
