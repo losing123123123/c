1c99c4560513185e07bcaecb642c2ca3d021023c
losing123123123 <106807901+losing123123123@users.noreply.github.com>
2022-11-25 10:36:12 +0800

    Add files via upload

--- 202211/22/main.c
+++ 202211/22/main.c
@@ -1,9 +1,60 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
+#include<stdlib.h>
 
+
+typedef struct sinlist
+{
+	int data;
+	struct sinlist* next;
+}sl;
+
+void PrintSList(sl* phead)
+{
+	while (phead)
+	{
+		printf("%d ", phead->data);
+		phead = phead->next;
+	}
+}
+
+sl* test(sl * p)
+{
+	sl* slow = p;
+	sl* fast = p;
+	while (fast != NULL && fast->next != NULL)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+	return slow;
+}
+
+//找到单链表的中间元素
 int main()
 {
+	sl* n1 = (sl*)malloc(sizeof(sl));
+	sl* n2 = (sl*)malloc(sizeof(sl));
+	sl* n3 = (sl*)malloc(sizeof(sl));
+	sl* n4 = (sl*)malloc(sizeof(sl));
+	sl* n5 = (sl*)malloc(sizeof(sl));
+	sl* n6 = (sl*)malloc(sizeof(sl));
 
+	n1->data = 1;
+	n2->data = 2;
+	n3->data = 3;
+	n4->data = 4;
+	n5->data = 5;
+	n6->data = 6;
+
+	n1->next = n2;
+	n2->next = n3;
+	n3->next = n4;
+	n4->next = n5;
+	n5->next = n6;
+	n6->next = NULL;
+
+	printf("%d", test(n1)->data);
 
 	return 0;
 }
