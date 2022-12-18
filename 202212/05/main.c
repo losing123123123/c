d7e3ac72e94e7d3d31fd76b089cc53408c2e72cb
losing123123123 <106807901+losing123123123@users.noreply.github.com>
2022-12-18 22:26:18 +0800

    Add files via upload

--- 202212/05/main.c
+++ 202212/05/main.c
@@ -1,9 +1,20 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
+//7-15 ¼ÆËãÔ²ÖÜÂÊ
 
 int main()
 {
-
-
-	return 0;
+    double input = 0;
+    double sum = 1;
+    int i = 1;
+    double j = 1.0;
+    scanf("%lf", &input);
+    while (j >= input)
+    {
+        j = j * i / (2 * i + 1);
+        sum += j;
+        i++;
+    }
+    printf("%lf", sum * 2);
+    return 0;
 }
